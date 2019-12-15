aff63e8c5cec9aa89b528bffcf0dba05c156d150
michael <zdong2010@gmail.com>
2019-12-15 13:05:11 +0800

    update route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostComponent } from './modules/post/post.component';
+import { PostDetailsComponent } from './modules/post/component/post-details/post-details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'posts', component: PostComponent},
+  {path: 'posts/:id', component: PostDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
