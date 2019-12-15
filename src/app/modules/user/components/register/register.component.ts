2db9a902d5403ee7651a171443666560b7a741b9
michael <zdong2010@gmail.com>
2019-12-15 16:09:46 +0800

    use formgroup, ngsubmit, validators,formbuilder

--- src/app/modules/user/components/register/register.component.ts
+++ src/app/modules/user/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { timingSafeEqual } from 'crypto';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  // registerForm = new FormGroup({
+  //   username: new FormControl('', Validators.required),
+  //   password: new FormControl('', [Validators.required, Validators.minLength(5)])
+  // });
+  registerForm = this.formBuilder.group({
+    username: ['', Validators.required],
+    password: ['',[Validators.required, Validators.minLength(6)]]
+  })
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.registerForm.valueChanges.subscribe(item => {
+      console.log(item);
+    });
+    // this.registerForm.setValue({
+    //   username: 'Bruce_Lee',
+    //   password: '11111'
+    // });
+    this.registerForm.statusChanges.subscribe(status => {
+      console.log('Status', status)
+    });
   }
-
+  onSubmit() {
+    console.log(this.registerForm.value);
+  }
 }
--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms'
 import { RegisterComponent } from './components/register/register.component';
 
 
@@ -7,7 +8,8 @@ import { RegisterComponent } from './components/register/register.component';
 @NgModule({
   declarations: [RegisterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ]
 })
 export class UserModule { }
