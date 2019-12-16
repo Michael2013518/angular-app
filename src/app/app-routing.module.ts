ed561d7a28285ce64262df1037ab4b56e442781e
michael <zdong2010@gmail.com>
2019-12-16 08:39:38 +0800

    拆封路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PostComponent } from './modules/post/post.component';
-import { PostDetailsComponent } from './modules/post/component/post-details/post-details.component';
 import { RegisterComponent } from './modules/user/components/register/register.component';
+import { PageNotFoundComponent } from './core/component/page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
-  {path: 'posts', component: PostComponent},
-  {path: 'posts/:id', component: PostDetailsComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: '', redirectTo: '/posts', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
---
+++ src/app/modules/post/post-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PostComponent } from './post.component';
+import { PostDetailsComponent } from './component/post-details/post-details.component';
+
+const routes: Routes = [
+  {path: 'posts', component: PostComponent},
+  {path: 'posts/:id', component: PostDetailsComponent}]
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
+})
+export class PostRoutingModule { }
