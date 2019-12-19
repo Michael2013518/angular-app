6a5f01067875da14bedf205a1c76a78f9c49c716
michael <zdong2010@gmail.com>
2019-12-19 22:01:55 +0800

    update app routing and module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './modules/user/components/register/register.component';
 import { PageNotFoundComponent } from './core/component/page-not-found/page-not-found.component';
+import { MessageBoxComponent } from './core/components/message-box/message-box.component';
 
 
 const routes: Routes = [
   {path: '', redirectTo: '/posts', pathMatch: 'full'},
-  {path: '**', component: PageNotFoundComponent}
+  {path: '**', component: PageNotFoundComponent },
+  {
+    path: 'message',
+    component: MessageBoxComponent,
+    outlet: 'popup',
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,22 @@ import { PostModule } from './modules/post/post.module';
 import { UserModule } from './modules/user/user.module';
 import { HeaderComponent } from './core/component/header/header.component';
 import { PageNotFoundComponent } from './core/component/page-not-found/page-not-found.component';
+import { DemoModule } from './modules/demo/demo.module';
+import { MessageBoxComponent } from './core/components/message-box/message-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    MessageBoxComponent
   ],
   imports: [
     BrowserModule,
     PostModule,
     UserModule,
-    AppRoutingModule
+    DemoModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
