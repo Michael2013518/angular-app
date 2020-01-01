7b7e2ac7a4baedc9b0776b81352cb1d3f2961e73
michael <zdong2010@gmail.com>
2020-01-01 22:24:07 +0800

    使用resolve提前获取数据

---
+++ src/app/modules/post/service/post-detail-resolve.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { Post } from '../models/post.model';
+import { PostService } from '../../posts/services/post.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostDetailResolveService implements Resolve<Post> {
+
+  constructor(private postService: PostService) { }
+  resolve(route: ActivatedRouteSnapshot): Post {
+    console.log('Post detail resolve service');
+    const postId = +route.paramMap.get('id');
+    return this.postService.show(postId);
+  }
+}
