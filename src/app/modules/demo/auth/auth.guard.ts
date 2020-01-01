9d46570c9a2c32b5dd2a41fa71dee7bce9abf07c
michael <zdong2010@gmail.com>
2020-01-01 22:27:11 +0800

    守卫路和子路由

---
+++ src/app/modules/demo/auth/auth.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router, CanActivateChild } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate, CanActivateChild {
+
+  constructor(private authService: AuthService, private router: Router) {
+
+  }
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    console.log('auth guard');
+    if (this.authService.isLoggedIn) {
+      return true;
+    }
+    this.router.navigate(['/demo/login']);
+  }
+  canActivateChild(
+    childRoute: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot):
+    boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    console.log('auth guard canactivechild');
+    return true;
+  }
+}
---
+++ src/app/modules/demo/auth/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  isLoggedIn = false;
+  constructor() { }
+  login() {
+    this.isLoggedIn = true;
+    console.log('logged in');
+  }
+  logout() {
+    this.isLoggedIn = false;
+    console.log('Logged out');
+  }
+}
