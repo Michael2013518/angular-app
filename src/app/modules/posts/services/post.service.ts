8ad07a3bac5d30edfa00f9396573c7c6b99a5720
michael <zdong2010@gmail.com>
2020-01-04 22:41:34 +0800

    handle error and retry get method

--- src/app/modules/posts/services/post.service.ts
+++ src/app/modules/posts/services/post.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Post } from '../../post/models/post.model';
 import { HttpClient } from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { catchError, retry, retryWhen, delay, take } from 'rxjs/operators';
 
 /**
  * providedIn: 'root' 可以在应用的任何地方使用
@@ -15,10 +17,22 @@ export class PostService {
   constructor(private http: HttpClient) {
   }
   index() {
-    return this.http.get(this.POST_API);
+    // 使用pipe
+    return this.http.get(this.POST_API).pipe(
+      catchError(this.handleError),
+      // retry(3),
+      retryWhen(errors =>
+        errors.pipe(
+          delay(3000),
+          take(3),
+        ))
+      );
   }
   show(id: number) {
     // return this.entities.find(item => item.id === id);
     return this.http.get<Post>(`${ this.POST_API }/${ id }`);
+  }
+  handleError() {
+    return throwError('Something went wrong!');
   }
 }
