602471664f43129897db298e0d4da9eac6bd394e
michael <zdong2010@gmail.com>
2020-01-04 23:54:10 +0800

    add service method

--- src/app/modules/posts/services/post.service.ts
+++ src/app/modules/posts/services/post.service.ts
@@ -13,6 +13,7 @@ import { catchError, retry, retryWhen, delay, take } from 'rxjs/operators';
 export class PostService {
   // json api
   POST_API = 'http://localhost:3000/posts';
+  MYLIST_API = 'http://localhost:3000/my-list';
 
   constructor(private http: HttpClient) {
   }
@@ -34,5 +35,17 @@ export class PostService {
   }
   handleError() {
     return throwError('Something went wrong!');
+  }
+  addToList(entity: Post) {
+    return this.http.post<Post>(this.MYLIST_API, entity);
+  }
+  getMyList() {
+    return this.http.get<Post[]>(this.MYLIST_API);
+  }
+  removeItemFromList(entityId: number) {
+    return this.http.delete(`${ this.MYLIST_API }/${ entityId }`);
+  }
+  update(id: number, entity: Post) {
+    return this.http.put<Post>(`${ this.POST_API }/${ id }`, entity);
   }
 }
