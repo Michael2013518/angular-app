225e333c7e3a0296388f3ac5a8221dc16ff118c6
michael <zdong2010@gmail.com>
2020-01-04 21:55:05 +0800

    http请求数据，单个内容资源

--- src/app/modules/post/service/post-detail-resolve.service.ts
+++ src/app/modules/post/service/post-detail-resolve.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
 import { Post } from '../models/post.model';
 import { PostService } from '../../posts/services/post.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ import { PostService } from '../../posts/services/post.service';
 export class PostDetailResolveService implements Resolve<Post> {
 
   constructor(private postService: PostService) { }
-  resolve(route: ActivatedRouteSnapshot): Post {
+  resolve(route: ActivatedRouteSnapshot): Observable<Post> {
     console.log('Post detail resolve service');
     const postId = +route.paramMap.get('id');
     return this.postService.show(postId);
--- src/app/modules/posts/services/post.service.ts
+++ src/app/modules/posts/services/post.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Post } from '../../post/models/post.model';
-import { posts } from '../../post/posts';
+import { HttpClient } from '@angular/common/http';
 
 /**
  * providedIn: 'root' 可以在应用的任何地方使用
@@ -9,14 +9,16 @@ import { posts } from '../../post/posts';
   providedIn: 'root'
 })
 export class PostService {
-  entities: Post[];
-  constructor() {
-    this.entities = posts;
+  // json api
+  POST_API = 'http://localhost:3000/posts';
+
+  constructor(private http: HttpClient) {
   }
   index() {
-    return this.entities;
+    return this.http.get(this.POST_API);
   }
   show(id: number) {
-    return this.entities.find(item => item.id === id);
+    // return this.entities.find(item => item.id === id);
+    return this.http.get<Post>(`${ this.POST_API }/${ id }`);
   }
 }
